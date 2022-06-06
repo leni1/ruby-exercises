e75ca8b745cf84f8e742ddd924204f163c53fcf6
Leni Kadali Mutungi <leni1@users.noreply.github.com>
2022-06-06 19:49:57 +0300

    Add completed nested collection exercises, specs

--- ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
+++ ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
@@ -1,3 +1,5 @@
+require 'pry-byebug'
+
 def blank_seating_chart(number_of_rows, seats_per_row)
   # return a 2d array to represent a seating chart that contains
   # number_of_rows nested arrays, each with seats_per_row entries of nil to
@@ -11,21 +13,27 @@ def blank_seating_chart(number_of_rows, seats_per_row)
 
   # NOTE: if one of the nested arrays is changed, the others should **not**
   # change with it
+  Array.new(number_of_rows) { Array.new(seats_per_row) }
 end
 
 def add_seat_to_row(chart, row_index, seat_to_add)
   # take a chart (2d array)  and add seat_to_add to the end of the row that is
   # at row_index index of the chart, then return the chart
+  chart[row_index].push(seat_to_add)
+  chart
 end
 
 def add_another_row(chart, row_to_add)
   # take a chart and add row_to_add to the end of the chart,
   # then return the chart.
+  chart.push(row_to_add)
 end
 
 def delete_seat_from_row(chart, row_index, seat_index)
   # take a chart and delete the seat at seat_index of the row at row_index of
   # the chart, then return the chart
+  chart[row_index].delete_at(seat_index)
+  chart
 
   # Hint: explore the ruby docs to find a method for deleting from an array!
 end
@@ -33,12 +41,20 @@ end
 def delete_row_from_chart(chart, row_index)
   # take a chart and delete the row at row_index of the chart,
   # then return the chart
+  chart.delete_at(row_index)
+  chart
 end
 
 def count_empty_seats(chart)
   # take a chart and return the number of empty (nil) seats in it
 
   # NOTE: `chart` should **not** be mutated
+  total_count = 0
+  chart.each do |seats|
+    seat_count = seats.count { |seat| seat.nil? }
+    total_count += seat_count
+  end
+  total_count
 end
 
 def find_favorite(array_of_hash_objects)
@@ -52,7 +68,12 @@ def find_favorite(array_of_hash_objects)
   #   { name: 'JavaScript', is_my_favorite?: false },
   #   { name: 'HTML', is_my_favorite?: false }
   # ]
-
+  array_of_hash_objects.each do |hash_object|
+    if hash_object[:is_my_favorite?] == true
+      return hash_object
+    end
+  end
+  return nil
   # TIP: there will only be a maximum of one hash in the array that will
   # return true to the :is_my_favorite? key
 end
